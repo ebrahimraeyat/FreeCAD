3b456ab70d9ae751c6d2292e9175f036fda3bfb1
Bernd Hahnebach <bernd@bimstatik.org>
2020-04-13 06:45:38 +0200

    FEM: OpenSees solver, add object and all FEM object unit tests

--- src/Mod/Fem/femtest/app/test_object.py
+++ src/Mod/Fem/femtest/app/test_object.py
@@ -113,6 +113,7 @@ class TestObjectCreate(unittest.TestCase):
         analysis.addObject(ObjectsFem.makeSolverCalculixCcxTools(doc))
         analysis.addObject(ObjectsFem.makeSolverCalculix(doc))
         sol = analysis.addObject(ObjectsFem.makeSolverElmer(doc))[0]
+        analysis.addObject(ObjectsFem.makeSolverOpenSees(doc))
         analysis.addObject(ObjectsFem.makeSolverZ88(doc))
 
         ObjectsFem.makeEquationElasticity(doc, sol)
@@ -354,6 +355,10 @@ class TestObjectType(unittest.TestCase):
             type_of_obj(solverelmer)
         )
         self.assertEqual(
+            "Fem::FemSolverObjectOpenSees",
+            type_of_obj(ObjectsFem.makeSolverOpenSees(doc))
+        )
+        self.assertEqual(
             "Fem::FemSolverObjectZ88",
             type_of_obj(ObjectsFem.makeSolverZ88(doc))
         )
@@ -551,6 +556,10 @@ class TestObjectType(unittest.TestCase):
         self.assertTrue(is_of_type(
             solverelmer,
             "Fem::FemSolverObjectElmer"
+        ))
+        self.assertTrue(is_of_type(
+            ObjectsFem.makeSolverOpenSees(doc),
+            'Fem::FemSolverObjectOpenSees'
         ))
         self.assertTrue(is_of_type(
             ObjectsFem.makeSolverZ88(doc),
@@ -1183,6 +1192,25 @@ class TestObjectType(unittest.TestCase):
             "Fem::FemSolverObjectElmer"
         ))
 
+        # FemSolverObjectOpenSees
+        solver_opensees = ObjectsFem.makeSolverOpenSees(doc)
+        self.assertTrue(is_derived_from(
+            solver_opensees,
+            'App::DocumentObject'
+        ))
+        self.assertTrue(is_derived_from(
+            solver_opensees,
+            'Fem::FemSolverObject'
+        ))
+        self.assertTrue(is_derived_from(
+            solver_opensees,
+            'Fem::FemSolverObjectPython'
+        ))
+        self.assertTrue(is_derived_from(
+            solver_opensees,
+            'Fem::FemSolverObjectOpenSees'
+        ))
+
         # FemSolverObjectZ88
         solver_z88 = ObjectsFem.makeSolverZ88(doc)
         self.assertTrue(is_derived_from(
@@ -1483,6 +1511,11 @@ class TestObjectType(unittest.TestCase):
         )
         self.assertTrue(
             solverelmer.isDerivedFrom("Fem::FemSolverObjectPython")
+        )
+        self.assertTrue(
+            ObjectsFem.makeSolverOpenSees(
+                doc
+            ).isDerivedFrom('Fem::FemSolverObjectPython')
         )
         self.assertTrue(
             ObjectsFem.makeSolverZ88(
